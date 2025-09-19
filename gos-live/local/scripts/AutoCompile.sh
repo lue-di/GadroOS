13591f0339ac3a25d60846f5e23c62ad397e6525
luedi <wallisluedi@gmail.com>
2025-09-19 19:44:25 +0800

    update cicd

--- gos-live/local/scripts/AutoCompile.sh
+++ gos-live/local/scripts/AutoCompile.sh
@@ -7,6 +7,10 @@ apt-get update && apt-get upgrade -y
 apt-get install -y --no-install-recommends live-build debootstrap  systemd-sysv iputils-ping curl  curl gpg ca-certificates apt-transport-https wget
 update-ca-certificates
 apt-get update -y
+echo "=== 根证书列表 (/etc/ssl/certs) ==="
+for cert in /etc/ssl/certs/*.crt; do
+    echo "文件: $cert"
+done
 cd /workspace || exit
 echo '==> Enter /workspace '
 
